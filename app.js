41d45b24619d1ff2269c146ad68858581a6117df
Adam Smith <coloradoasmith@gmail.com>
2020-12-16 16:59:48 -0700

    getting up to part 5 installed cross-env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require("./utils/config");
 const express = require("express");
 require("express-async-errors");
 require("dotenv").config();
+const bodyParser = require('body-parser')
 
 const app = express();
 const cors = require("cors");
@@ -30,12 +31,18 @@ mongoose
 
 app.use(cors());
 app.use(express.static("build"));
+app.use(bodyParser.json())
+
 app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === 'test') {  
+  const testingRouter = require('./controllers/testing') 
+  app.use('/api/testing', testingRouter)}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
